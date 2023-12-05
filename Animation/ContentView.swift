f82b86d890763e65a9a3ecd793bcbda5751dcf13
joy jain <joyshah204@gmial.com>
2023-12-05 19:08:19 +0530

    complete

--- Animation/ContentView.swift
+++ Animation/ContentView.swift
@@ -9,13 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Button("Tap Me") {
+            // do nothing
         }
-        .padding()
+        .padding(50)
+        .background(.red)
+        .foregroundStyle(.white)
+        .clipShape(.circle)
     }
 }
 
